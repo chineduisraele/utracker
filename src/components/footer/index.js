9cabdee6d398bb1c278aadbffd95d80091e9548a
chineduisraele <chineduisraele@gmail.com>
2022-05-26 02:23:10 +0100

    finished with footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram, FaGlobe } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+  FaGlobe,
+} from "react-icons/fa";
 import "./sass/index.css";
 
 const Footer = () => {
@@ -8,7 +15,7 @@ const Footer = () => {
       <footer className="one">
         <div className="inner">
           {/* About */}
-          <div className="first">
+          <div className="about">
             <h3>OUR COMPANY</h3>
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi
@@ -18,23 +25,31 @@ const Footer = () => {
             </p>
           </div>
           {/* follow */}
-          <div className="second d-flex aic">
-            <div className="follow">
+          <div className="follow d-flex aic">
+            <div className="followlinks d-grid">
               <h3>FOLLOW COMPANY</h3>
-              <span>
-                <FaFacebook />
-              </span>
-              <span>
-                <FaTwitter />
-              </span>
-              <span>
-                <FaInstagram />
-              </span>
+              <div>
+                <a href="#">
+                  <FaFacebook style={{ color: "#0e5998" }} />
+                </a>
+                <a href="#">
+                  <FaTwitter style={{ color: "#2ea8e0" }} />
+                </a>
+                <a href="#">
+                  <FaInstagram style={{ color: "#2ea8e0" }} />
+                </a>
+                <a href="#">
+                  <FaLinkedin style={{ color: "#077bb8" }} />
+                </a>
+                <a href="#">
+                  <FaYoutube style={{ color: "#eb2227" }} />
+                </a>
+              </div>
             </div>
-            <div className="language">
-              <h3>LANGUAGE</h3>
+            <div className="country">
+              <h3>COUNTRY</h3>
               <span>
-                <FaGlobe /> Nigeria
+                <FaGlobe /> Nigeria, Ghana, Kenya, UK, USA
               </span>
             </div>
           </div>
@@ -43,8 +58,8 @@ const Footer = () => {
 
       {/* two */}
       <footer className="two">
-        <div className="third d-flex aic">
-          <span>&copy; company name 2022</span>
+        <div className="inner d-flex aic">
+          <span>&copy; uTracker 2022</span>
           <span>Terms of Use</span>
           <span>Security and Privacy</span>
         </div>
